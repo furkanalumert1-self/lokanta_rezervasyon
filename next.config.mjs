a5b78300e1ee43f774f19a670c46004956f76d67
furkanalumert1-self <furkanalumert1@gmail.com>
2026-04-17 22:47:20 +0300

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["@prisma/client", "prisma"],
-  },
-};
-export default nextConfig;
